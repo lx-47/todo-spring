0a194b83f4a554e2dc56a1e585b16090fd34d74e
Alex Varghese <alexkvpius47@gmail.com>
2025-03-24 10:43:33 +0530

    Update TodoRequest.java

--- src/main/java/com/example/todo/dto/TodoRequest.java
+++ src/main/java/com/example/todo/dto/TodoRequest.java
@@ -1,24 +1,22 @@
 package com.example.todo.dto;
 
 public class TodoRequest {
-    private String Title;
-    private String Description;
+    private String title;
+    private String description;
 
-    // Getters
     public String getTitle() {
-        return Title;
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getDescription() {
-        return Description;
-    }
-
-    // Setters
-    public void setTitle(String title) {
-        this.Title = title;
+        return description;
     }
 
     public void setDescription(String description) {
-        this.Description = description;
+        this.description = description;
     }
 }
