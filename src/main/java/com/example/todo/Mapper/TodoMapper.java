54f32a215d5fdfca47da01d811a4d66a421a23a3
Alex Varghese <alexkvpius47@gmail.com>
2025-03-24 10:46:28 +0530

    Update TodoMapper.java

--- src/main/java/com/example/todo/Mapper/TodoMapper.java
+++ src/main/java/com/example/todo/Mapper/TodoMapper.java
@@ -3,15 +3,17 @@ package com.example.todo.Mapper;
 
 import com.example.todo.dto.TodoResponse;
 import com.example.todo.entity.TodoEntity;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TodoMapper {
-    public static TodoResponse todoResponse(TodoEntity todoEntity){
-        return new TodoResponse(
-                todoEntity.getId(),
-                todoEntity.getTitle(),
-                todoEntity.getDescription(),
-                todoEntity.isCompleted(),
-                todoEntity.getCreatedAt()
-        );
+    public TodoResponse todoResponse(TodoEntity todoEntity) {
+        TodoResponse response = new TodoResponse();
+        response.setId(todoEntity.getId());
+        response.setTitle(todoEntity.getTitle());
+        response.setDescription(todoEntity.getDescription());
+        response.setCompleted(todoEntity.isCompleted());
+        response.setCreatedAt(todoEntity.getCreatedAt());
+        return response;
     }
 }
